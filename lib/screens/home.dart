32e8c8b42abd50e116d6ef27e3847c1a03c7c465
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-10-02 00:32:29 -0400

    added filter dropdown, and dynamic color to warnings + filters

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,44 @@ import '../services/warningservice.dart';
 import '../model/warning.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+String? _selectedType;
+String? _selectedColor;
+String? _selectedSeverity;
+DateTime? _selectedDate;
+
+Widget _buildDropdown(String label, List<String> items, String? selectedValue, Function(String?) onChanged) {
+  return DropdownButton<String>(
+    hint: Row(
+      children: [
+        const Icon(Icons.search),
+        const SizedBox(width: 10),
+        Text(label),
+      ],
+    ),
+    value: selectedValue,
+    isExpanded: true,
+    onChanged: onChanged,
+    items: items.map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Row(
+          children: [
+            Text(value),
+          ],
+        ),
+      );
+    }).toList(),
+  );
+}
+Widget _buildFilterChip(String label, String? value, Function() onRemove) {
+  return value != null
+      ? Chip(
+          label: Text('$label: $value'),
+          deleteIcon: const Icon(Icons.close),
+          onDeleted: onRemove,
+        )
+      : Container();
+}
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -14,25 +52,62 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _warningService = WarningService();
   Future<Map<String, dynamic>> _warningsFuture = Future.value({});
+  bool _isFilterExpanded = false;
+  
 
   @override
   void initState() {
     super.initState();
     _loadTokenAndFetchWarnings();
   }
-
+  Widget _buildFilters() {
+  return Wrap(
+    spacing: 8.0,
+    runSpacing: 4.0,
+    children: <Widget>[
+      _buildFilterChip('Color', _selectedColor, () {
+        setState(() {
+          _selectedColor = null;
+          _loadTokenAndFetchWarnings();
+        });
+      }),
+      _buildFilterChip('Type', _selectedType, () {
+        setState(() {
+          _selectedType = null;
+          _loadTokenAndFetchWarnings();
+        });
+      }),
+      _buildFilterChip('Severity', _selectedSeverity, () {
+        setState(() {
+          _selectedSeverity = null;
+          _loadTokenAndFetchWarnings();
+        });
+      }),
+      _buildFilterChip('Date', _selectedDate?.toIso8601String(), () {
+        setState(() {
+          _selectedDate = null;
+          _loadTokenAndFetchWarnings();
+        });
+      }),
+    ],
+  );
+}
   Future<void> _loadTokenAndFetchWarnings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString('token') ?? '';
-    _warningsFuture = _warningService.getWarnings(token);
+    _warningsFuture = _warningService.getWarnings(token,
+      type: _selectedType,
+      color: _selectedColor,
+      severity: _selectedSeverity,
+      date: _selectedDate,);
     setState(() {});
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
+        backgroundColor: Colors.blueGrey,
       ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: _warningsFuture,
@@ -43,6 +118,9 @@ class _HomeScreenState extends State<HomeScreen> {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             List<Warning> warnings = snapshot.data!['warnings'];
+            Set<String> colorItems = warnings.map((warning) => warning.color).toSet();
+            Set<String> typeItems = warnings.map((warning) => warning.type).toSet();
+            Set<String> severityItems = warnings.map((warning) => warning.severity).toSet();
             return Column(
               children: [
                 Card(
@@ -51,55 +129,89 @@ class _HomeScreenState extends State<HomeScreen> {
                     title: Text('Total Warnings: ${warnings.length}', style: const TextStyle(fontWeight: FontWeight.bold)),
                   ),
                 ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: DataTable(
-                    columns: const <DataColumn>[
-                      DataColumn(
-                        label: Text('Color'),
-                      ),
-                      DataColumn(
-                        label: Text('Type'),
-                      ),
-                      DataColumn(
-                        label: Text('Duration Type'),
-                      ),
-                      DataColumn(
-                        label: Text('Duration Value'), 
-                      ),
-                      DataColumn(
-                        label: Text('Engine Hours Value'), 
-                      ),
-                      DataColumn(
-                        label: Text('Latitude'), 
-                      ),
-                      DataColumn(
-                        label: Text('Longitude'), 
-                      ),
-                      // Add more DataColumn for each field
-                    ],
-                    rows: warnings.map<DataRow>((Warning warning) {
-                      return DataRow(
-                        selected: warning.ignored,
-                        onSelectChanged: (bool? value) {
-                          setState(() {
-                            warning.selected = value!;
-                          });
-                        },
-                        cells: <DataCell>[
-                          DataCell(Text(warning.color)),
-                          DataCell(Text(warning.type)),
-                          DataCell(Text(warning.durationType)),
-                          DataCell(Text(warning.durationValue.toString())), 
-                          DataCell(Text(warning.engineHoursValue.toString())), 
-                          DataCell(Text(warning.lat.toString())), 
-                          DataCell(Text(warning.lon.toString())), 
-                          // Add more DataCell for each field
+                _buildFilters(),
+                ExpansionPanelList(
+                  expansionCallback: (int index, bool isExpanded) {
+                    setState(() {
+                      _isFilterExpanded = !_isFilterExpanded;
+                    });
+                  },
+                  children: [
+                    ExpansionPanel(
+                      headerBuilder: (BuildContext context, bool isExpanded) {
+                        return const ListTile(
+                          title: Text('Filters'),
+                        );
+                      },
+                      body: Column(
+                        children: <Widget>[
+                          _buildDropdown('Color', colorItems.toList(), _selectedColor, (value) {
+                            setState(() {
+                              _selectedColor = value;
+                              _loadTokenAndFetchWarnings();
+                            });
+                          }),
+                          _buildDropdown('Type', typeItems.toList(), _selectedType, (value) {
+                            setState(() {
+                              _selectedType = value;
+                              _loadTokenAndFetchWarnings();
+                            });
+                          }),
+                          _buildDropdown('Severity', severityItems.toList(), _selectedSeverity, (value) {
+                            setState(() {
+                              _selectedSeverity = value;
+                              _loadTokenAndFetchWarnings();
+                            });
+                          }),
                         ],
+                      ),
+                      isExpanded: _isFilterExpanded,
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: warnings.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Colors.white,
+                        child: ExpansionPanelList(
+                          expansionCallback: (int item, bool status) {
+                            setState(() {
+                              warnings[index].isExpanded = !warnings[index].isExpanded;
+                            });
+                          },
+                          children: [
+                            ExpansionPanel(
+                              headerBuilder: (BuildContext context, bool isExpanded) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: getColorFromWarning(warnings[index].color),
+                                      width: 2,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: ListTile(
+                                    title: Text(
+                                      'Color: ${warnings[index].color}, ID: ${warnings[index].id}, Type: ${warnings[index].type}',
+                                      style: const TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                );
+                              },
+                              body: ListTile(
+                                title: Text(
+                                  'Duration Type: ${warnings[index].durationType}, Duration Value: ${warnings[index].durationValue}, Engine Hours Value: ${warnings[index].engineHoursValue}, Latitude: ${warnings[index].lat}, Longitude: ${warnings[index].lon}',
+                                  style: const TextStyle(color: Colors.black),
+                                ),
+                              ),
+                              isExpanded: warnings[index].isExpanded,
+                            ),
+                          ],
+                        ),
                       );
-                    }).toList(),
-                  ),
+                    },
                   ),
                 ),
               ],
@@ -108,5 +220,18 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       ),
     );
+  }
+
+  Color getColorFromWarning(String color) {
+    switch (color.toUpperCase()) {
+      case 'RED':
+        return Colors.red;
+      case 'YELLOW':
+        return Colors.yellow;
+      case 'BLUE':
+        return Colors.blue;
+      default:
+        return Colors.grey;
+    }
   }
 }
