158fec8a867a9674f8d6da0ce7dc62ea8c5a7b7b
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-10-03 23:58:22 -0400

    major visual changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 // lib/screens/home.dart
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../services/warningservice.dart';
 import '../model/warning.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -60,6 +61,42 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     _loadTokenAndFetchWarnings();
   }
+  Widget buildWarningCard(Map<String, int> warningCounts) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: warningCounts.entries.map((entry) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 5,
+            child: SizedBox(
+              height: 100,
+              width: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.warning,
+                    size: 40,
+                    color: getColorFromWarning(entry.key),
+                  ),
+                  Text(
+                    '${entry.value}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
   Widget _buildFilters() {
   return Wrap(
     spacing: 8.0,
@@ -104,10 +141,37 @@ class _HomeScreenState extends State<HomeScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    Map<String, int> warningCounts = { // testing purposes delete later
+      'Red': 5,
+      'Blue': 1,
+    };
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
-        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Alerts',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'assets/logo.png',
+            ),
+          ),
+        ],
+        backgroundColor: const Color(0xFFE9E9E9),
       ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: _warningsFuture,
@@ -123,90 +187,105 @@ class _HomeScreenState extends State<HomeScreen> {
             Set<String> severityItems = warnings.map((warning) => warning.severity).toSet();
             return Column(
               children: [
-                Card(
-                  elevation: 5,
-                  child: ListTile(
-                    title: Text('Total Warnings: ${warnings.length}', style: const TextStyle(fontWeight: FontWeight.bold)),
-                  ),
-                ),
-                _buildFilters(),
-                ExpansionPanelList(
-                  expansionCallback: (int index, bool isExpanded) {
-                    setState(() {
-                      _isFilterExpanded = !_isFilterExpanded;
-                    });
-                  },
-                  children: [
-                    ExpansionPanel(
-                      headerBuilder: (BuildContext context, bool isExpanded) {
-                        return const ListTile(
-                          title: Text('Filters'),
-                        );
-                      },
-                      body: Column(
-                        children: <Widget>[
-                          _buildDropdown('Color', colorItems.toList(), _selectedColor, (value) {
-                            setState(() {
-                              _selectedColor = value;
-                              _loadTokenAndFetchWarnings();
-                            });
-                          }),
-                          _buildDropdown('Type', typeItems.toList(), _selectedType, (value) {
-                            setState(() {
-                              _selectedType = value;
-                              _loadTokenAndFetchWarnings();
-                            });
-                          }),
-                          _buildDropdown('Severity', severityItems.toList(), _selectedSeverity, (value) {
-                            setState(() {
-                              _selectedSeverity = value;
-                              _loadTokenAndFetchWarnings();
-                            });
-                          }),
-                        ],
-                      ),
-                      isExpanded: _isFilterExpanded,
-                    ),
-                  ],
-                ),
+                buildWarningCard(warningCounts),
+                // _buildFilters(),
+                // ExpansionPanelList(
+                //   expansionCallback: (int index, bool isExpanded) {
+                //     setState(() {
+                //       _isFilterExpanded = !_isFilterExpanded;
+                //     });
+                //   },
+                //   children: [
+                //     ExpansionPanel(
+                //       headerBuilder: (BuildContext context, bool isExpanded) {
+                //         return const ListTile(
+                //           title: Text('Filters'),
+                //         );
+                //       },
+                //       body: Column(
+                //         children: <Widget>[
+                //           _buildDropdown('Color', colorItems.toList(), _selectedColor, (value) {
+                //             setState(() {
+                //               _selectedColor = value;
+                //               _loadTokenAndFetchWarnings();
+                //             });
+                //           }),
+                //           _buildDropdown('Type', typeItems.toList(), _selectedType, (value) {
+                //             setState(() {
+                //               _selectedType = value;
+                //               _loadTokenAndFetchWarnings();
+                //             });
+                //           }),
+                //           _buildDropdown('Severity', severityItems.toList(), _selectedSeverity, (value) {
+                //             setState(() {
+                //               _selectedSeverity = value;
+                //               _loadTokenAndFetchWarnings();
+                //             });
+                //           }),
+                //         ],
+                //       ),
+                //       isExpanded: _isFilterExpanded,
+                //     ),
+                //   ],
+                // ),
                 Expanded(
                   child: ListView.builder(
                     itemCount: warnings.length,
                     itemBuilder: (context, index) {
                       return Card(
-                        color: Colors.white,
-                        child: ExpansionPanelList(
-                          expansionCallback: (int item, bool status) {
-                            setState(() {
-                              warnings[index].isExpanded = !warnings[index].isExpanded;
-                            });
-                          },
-                          children: [
-                            ExpansionPanel(
-                              headerBuilder: (BuildContext context, bool isExpanded) {
-                                return Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      color: getColorFromWarning(warnings[index].color),
-                                      width: 2,
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Checkbox(
+                                    value: warnings[index].selected,
+                                    onChanged: (bool? value) {
+                                      setState(() {
+                                        warnings[index].selected = value;
+                                      });
+                                    },
+                                  )
+                                ],
+                              ),
+                              title: Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          warnings[index].definitionType,
+                                          style: TextStyle(
+                                            color: getColorFromWarning(warnings[index].color),
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(warnings[index].definitionDescription),
+                                        Text('Date: ${DateFormat('dd/MM/yyyy hh:mm').format(warnings[index].time)}'),
+                                      ],
                                     ),
-                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  child: ListTile(
-                                    title: Text(
-                                      'Color: ${warnings[index].color}, ID: ${warnings[index].id}, Type: ${warnings[index].type}',
-                                      style: const TextStyle(color: Colors.black),
-                                    ),
-                                  ),
-                                );
-                              },
-                              body: ListTile(
+                                ],
+                              ),
+                            ),
+                            Visibility(
+                              visible: warnings[index].isExpanded,
+                              child: ListTile(
                                 title: Text(
                                   'Duration Type: ${warnings[index].durationType}, Duration Value: ${warnings[index].durationValue}, Engine Hours Value: ${warnings[index].engineHoursValue}, Latitude: ${warnings[index].lat}, Longitude: ${warnings[index].lon}',
                                   style: const TextStyle(color: Colors.black),
                                 ),
                               ),
-                              isExpanded: warnings[index].isExpanded,
+                            ),
+                            TextButton(
+                              child: Text(warnings[index].isExpanded ? 'Hide Details' : 'Show Details'),
+                              onPressed: () {
+                                setState(() {
+                                  warnings[index].isExpanded = !warnings[index].isExpanded;
+                                });
+                              },
                             ),
                           ],
                         ),
@@ -225,13 +304,13 @@ class _HomeScreenState extends State<HomeScreen> {
   Color getColorFromWarning(String color) {
     switch (color.toUpperCase()) {
       case 'RED':
-        return Colors.red;
+        return const Color(0xFFF2392D);
       case 'YELLOW':
-        return Colors.yellow;
+        return const Color(0xFFFECC1C);
       case 'BLUE':
-        return Colors.blue;
+        return const Color(0xFF3E74FE);
       default:
-        return Colors.grey;
+        return Colors.black;
     }
   }
 }
